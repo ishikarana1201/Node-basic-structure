eb2c042dec632203ff3f59be175dd565bfe9036b
Yudiz Solutions Pvt. Ltd <yudiz@Yudizs-Mac-mini.local>
2022-11-16 14:43:03 +0530

    Add middleware types and code

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const { response } = require("express");
 const express = require("express");
+const reqFilter = require("./middleware");
 const path = require("path");
 
 const app = express();
 const publicPath = path.join(__dirname, "public");
 // console.log(publicPath)
 // app.use(express.static(publicPath));
+
+//Application-level middleware
+// app.use(reqFilter);
+// app.get("/", (_, res) => {
+//   res.send("This is home page");
+// });
+
+// // Single route middleware
+// app.get("/about", reqFilter, (_, res) => {
+//   res.send("This is about page");
+// });
+
+// // Specific page middleware
+// const route = express.Router();
+// route.use(reqFilter);
+
+// route.get("/contact", (req, res) => {
+//     res.send("This is contact page");
+//   });
+//   app.use('/',route);
+
+
+
+
 
 // Define route
 app.get("", (_, res) => {
